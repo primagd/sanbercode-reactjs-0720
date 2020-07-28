e51d48befb5ab55483ba84a6d49c3751a1be657f
primagd <p.gdhiharto24@gmail.com>
2020-07-28 08:31:42 +0700

    fix tugas

--- Pekan 2/Tugas Hari Ke-1/tugas6.js
+++ Pekan 2/Tugas Hari Ke-1/tugas6.js
@@ -1,120 +1,116 @@
-// //---------------Soal 1---------------
-// var arrayDaftarPeserta = ["Asep", "laki-laki", "baca buku", 1992]
-// var objectDaftarPeserta = {
-//     nama: "Asep",
-//     jeniskelamin: "laki-laki",
-//     hobi: "baca buku",
-//     "tahun lahir": 1992
-// }
+//---------------Soal 1---------------
+var arrayDaftarPeserta = ["Asep", "laki-laki", "baca buku", 1992]
+var objectDaftarPeserta = {
+    nama: "Asep",
+    jeniskelamin: "laki-laki",
+    hobi: "baca buku",
+    "tahun lahir": 1992
+}
 
-// //---------------Soal 2---------------
-// var data = [{
-//     "1.nama": "strawberry",
-//     warna: "merah",
-//     "ada bijinya": "tidak",
-//     harga: 9000
-// }, {
-//     "2.nama": "jeruk",
-//     warna: "oranye",
-//     "ada bijinya": "ada",
-//     harga: 8000
-// }, {
-//     "3.nama": "Semangka",
-//     warna: "Hijau & Merah",
-//     "ada bijinya": "ada",
-//     harga: 10000
-// }, {
-//     "4.nama": "Pisang",
-//     warna: "Kuning",
-//     "ada bijinya": "tidak",
-//     harga: 5000
-// }]
+//---------------Soal 2---------------
+var data = [{
+    "1.nama": "strawberry",
+    warna: "merah",
+    "ada bijinya": "tidak",
+    harga: 9000
+}, {
+    "2.nama": "jeruk",
+    warna: "oranye",
+    "ada bijinya": "ada",
+    harga: 8000
+}, {
+    "3.nama": "Semangka",
+    warna: "Hijau & Merah",
+    "ada bijinya": "ada",
+    harga: 10000
+}, {
+    "4.nama": "Pisang",
+    warna: "Kuning",
+    "ada bijinya": "tidak",
+    harga: 5000
+}]
 
-// console.log(data["0"])
+console.log(data["0"])
 
-// //---------------Soal 3---------------
-// var dataFilm = []
+//---------------Soal 3---------------
+var dataFilm = []
 
-// function namaFilm(nama, durasi, genre, tahun) {
-//     return dataFilm.push({ nama, durasi, genre, tahun });
-// }
+function namaFilm(nama, durasi, genre, tahun) {
+    return dataFilm.push({ nama, durasi, genre, tahun });
+}
 
-// // var nama = "a"
-// // var durasi = "b"
-// // var genre = "c"
-// // var tahun = "d"
 
-// namaFilm("Naruto", "45 Menit", "Anime", "2020")
-// namaFilm("Hinata", "45 Menit", "Anime", "2020")
-// console.log(dataFilm)
+namaFilm("Naruto", "45 Menit", "Anime", "2020")
+namaFilm("Hinata", "45 Menit", "Anime", "2020")
+console.log(dataFilm)
 
-// //---------------Soal 4---------------
+//---------------Soal 4---------------
 
-// class Animal {
-//     constructor(name) {
-//         this.name = "name";
-//         this.legs = 4;
-//         this.cold_blooded = false;
-//     }
+class Animal {
+    constructor(name) {
+        this.name = "name";
+        this.legs = 4;
+        this.cold_blooded = false;
+    }
 
-//     get getname() {
-//         return this.name;
-//     }
-//     get getlegs() {
-//         return this.legs;
-//     }
-//     get getcold_blooded() {
-//         return this.cold_blooded;
-//     }
-// }
+    get getname() {
+        return this.name;
+    }
+    get getlegs() {
+        return this.legs;
+    }
+    get getcold_blooded() {
+        return this.cold_blooded;
+    }
+}
 
-// var sheep = new Animal("shaun");
+var sheep = new Animal("shaun");
 
-// console.log(sheep.name) // "shaun"
-// console.log(sheep.legs) // 4
-// console.log(sheep.cold_blooded) // false
+console.log(sheep.name) // "shaun"
+console.log(sheep.legs) // 4
+console.log(sheep.cold_blooded) // false
 
 
 
 
-// // Code class Ape dan class Frog di sini
+// Code class Ape dan class Frog di sini
 
-// class Ape {
-//     constructor(item) {
-//         this.item = item;
-//         // this.yell = "Auooo";
-//     }
-//     get getitem() {
-//         return this.item;
-//     }
-//     yell(Auooo) {
-//             return "Auooo";
-//         }
-//         // get getyell() {
-//         //     return this.yell;
-// }
+class Ape {
+    constructor(item) {
+        this.item = item;
+        // this.yell = "Auooo";
+    }
+    get getitem() {
+        return this.item;
+    }
+    yell(Auooo) {
+            return "Auooo";
+        }
+        // get getyell() {
+        //     return this.yell;
+}
 
-// class Frog extends Animal {
-//     constructor(item) {
-//         super();
-//         this.item = item;
-//     }
-//     get getitem() {
-//         return this.item;
-//     }
-//     jump(hophop) {
-//         return "hop hop";
-//     }
-// }
+class Frog extends Animal {
+    constructor(item) {
+        super();
+        this.item = item;
+    }
+    get getitem() {
+        return this.item;
+    }
+    jump(hophop) {
+        return "hop hop";
+    }
+}
 
 
-// var sungokong = new Ape("kera sakti")
-// sungokong.yell() // "Auooo"
-// console.log(sungokong.yell())
+var sungokong = new Ape("kera sakti")
+sungokong.yell() // "Auooo"
+console.log(sungokong.yell())
 
-// var kodok = new Frog("buduk")
-// kodok.jump() // "hop hop"
-// console.log(kodok.jump())
+var kodok = new Frog("buduk")
+kodok.jump() // "hop hop"
+console.log(kodok.jump())
 
 // //---------------Soal 5---------------
 
