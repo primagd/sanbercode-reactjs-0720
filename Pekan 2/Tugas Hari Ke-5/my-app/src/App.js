c03c2f4050346e8490eee2237004233b29c25759
primagd <p.gdhiharto24@gmail.com>
2020-08-03 14:11:11 +0700

    Tugas 5 m2 fix

--- Pekan 2/Tugas Hari Ke-5/my-app/src/App.js
+++ Pekan 2/Tugas Hari Ke-5/my-app/src/App.js
@@ -3,39 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( 
-<div className="App">
-    <h1 style={{"text-align":"center"}}> Form Pembelian Buah</h1>
-    <div>
-        <div style={{"display": "inline-block", "width": "150px", "font-weight": "bold", "font-size": "16px"}}>
-            Nama Pelanggan
-        </div>
-        <input style={{"display": "inline-block", "type":"text", "name":"name"}}> <br><br>
+    return (
+        <div>
+            <h1 style={{"text-align":"center"}}> Form Pembelian Buah</h1>
+            <div>
+                <div style={{display: "inline-block", width: "150px", "font-weight": "bold", "font-size": "16px"}}>
+                    Nama Pelanggan
+                </div>
 
+                <input style={{"display": "inline-block", "type":"text", "name":"name"}}></input> <br></br> <br></br>
 
-        <div style={{display: block, "margin-bottom": "1em"}}>
-            <div style={{display: inline-block, width: "150px", "font-weight": bold, "font-size": "16px"}}>
-                Daftar Item
+                <div style={{display: "block", "margin-bottom": "1em"}}>
+                    <div style={{display: "inline-block", width: "150px", "font-weight": "bold", "font-size": "16px"}}>
+                        Daftar Item
+                </div>
+                        <div style={{"display": "inline-block"}}>
+                        <input type="checkbox"></input>
+                        <label for="fruit1">Semangka</label> <br></br>
+                        <input type="checkbox"></input>
+                        <label for="fruit2">Jeruk</label> <br></br>
+                        <input type="checkbox"></input>
+                        <label for="fruit3">Nanas</label> <br></br>
+                        <input type="checkbox"></input>
+                        <label for="fruit4">Salak</label> <br></br>
+                        <input type="checkbox"></input>
+                        <label for="fruit5">Anggur</label> <br></br> <br></br>
+                </div>
+                </div>
+                <button>
+                    <a href="index.html" style={{"text-decoration": "none", "color": "black"}}>Kirim</a>
+                </button>          
             </div>
-            <div style={{"display": "inline-block"}>
-                <input type="checkbox" id="fruit1" name="fruit1" value="semangka">
-                <label for="fruit1">Semangka</label><br>
-                <input type="checkbox" id="fruit2" name="fruit2" value="jeruk">
-                <label for="fruit2">Jeruk</label><br>
-                <input type="checkbox" id="fruit3" name="fruit3" value="nanas">
-                <label for="fruit3">Nanas</label><br>
-                <input type="checkbox" id="fruit4" name="fruit4" value="salak">
-                <label for="fruit4">Salak</label><br>
-                <input type="checkbox" id="fruit5" name="fruit5" value="anggur">
-                <label for="fruit5">Anggur</label><br>
-            </div>
         </div>
-        <button>
-            <a href="index.html" style={{"text-decoration": "none"}}>Kirim</a>
-          </button>
 
-    </div>
-</div>
     );
 }
 
